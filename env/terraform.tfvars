df36be43f5039631cb73c09bee94e56e7f8b2381
Mahesh Kumar Rathore <m.rathore176@gmail.com>
2026-05-24 00:15:07 +0530

    updated

--- env/terraform.tfvars
+++ env/terraform.tfvars
@@ -1,2 +1,17 @@
-project_name     = "demo"
-location         = "Central India"
+project_name = "demo"
+
+location = "Central India"
+
+environment = "dev"
+
+vnet_address_space = [
+  "10.0.0.0/16"
+]
+
+private_endpoint_subnet_address = [
+  "10.0.1.0/24"
+]
+
+key_vault_sku = "standard"
+
+enable_public_network_access = false
--- variable.tf
+++ variable.tf
@@ -7,3 +7,36 @@ variable "location" {
   default = "Central India"
 }
 
+variable "project_name" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "Central India"
+}
+
+variable "vnet_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "private_endpoint_subnet_address" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "key_vault_sku" {
+  type    = string
+  default = "standard"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "enable_public_network_access" {
+  type    = bool
+  default = false
+}
