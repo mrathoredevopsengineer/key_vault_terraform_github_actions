21cb93f0dfaa81fb172974cd4a94413ea047a823
mrathoredevopsengineer <148685072+mrathoredevopsengineer@users.noreply.github.com>
2026-05-23 22:07:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "azurerm_key_vault" "kv" {
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   sku_name                    = "standard"
+  tenant_id = data.azurerm_client_config.current.tenant_id
 
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
